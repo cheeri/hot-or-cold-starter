5d15512c74f161369fd6aeeddd5af1f76d8503d8
murali.ambekar@gmail.com <murali.ambekar@gmail.com>
2015-10-28 16:54:29 -0500

    Almost Finished

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 
+var myWrapper = wrapper();
+
+
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
+	
+
+  /*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
@@ -12,6 +17,92 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$(".new").click(function(){
+    	myWrapper.NewGame();
+
+  	});
+
+
+    $("#guessButton").click(function(){
+      
+      var guessedNumber = $('#userGuess').val();
+      guessedNumber = parseInt(guessedNumber,10); 
+        if (!isNaN(guessedNumber))
+            {
+                myWrapper.ProcessGuess(guessedNumber);
+            }            
+
+    }); 
+
 });
+
+
+function wrapper(){
+
+  var numberToBeGuessed;
+  var attemptsCounter;
+  var lastDifference;
+
+ function NewGame(){
+
+  	numberToBeGuessed = Math.floor((Math.random()*100)+1);
+  	attemptsCounter = 0;
+    lastDifference = 100;
+    $('#guessList').empty();
+	
+
+ };
+
+ function ProcessGuess(guessedNumber){
+  
+  attemptsCounter++;
+
+  if (guessedNumber === numberToBeGuessed){
+
+    $('#feedback').html('Bingo');
+    NewGame();
+
+  }
+
+  else
+  {
+    if (computeTheDifference(guessedNumber))
+    {
+      // getting closer
+      $('#feedback').html('Hotter');
+
+    } else {
+
+      //getting farther
+      $('#feedback').html('Colder');
+
+    };
+
+    $('#count').html(attemptsCounter); 
+    $('#guessList').append('<li><label>'+guessedNumber+'</label> </li>');
+
+  };  
+  
+};
+
+ function computeTheDifference(guessedNumber){
+
+  var currentDifference = Math.abs(guessedNumber - numberToBeGuessed);
+
+  if  (currentDifference < lastDifference)
+  {
+      lastDifference = currentDifference;
+      return true;
+
+  }else
+    {
+      lastDifference = currentDifference;
+      return false;
+    }
+  };
+
+  return{NewGame: NewGame, ProcessGuess:ProcessGuess};
+
+};
 
 
